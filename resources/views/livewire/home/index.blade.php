8fefe93ff91c4d4306a8be4560a59d81cefeefb0
Ronel028 <floridaronel15@gmail.com>
2024-09-09 19:27:37 +0800

    initial commit

---
+++ resources/views/livewire/home/index.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Hello! This boilerplate for Laravel | Livewire 3 and Tailwind css</h1>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use App\Livewire\Home\Index;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', Index::class);
